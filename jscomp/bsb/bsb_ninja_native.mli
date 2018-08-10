63380fb59ca1442edb96a7cf576067c241152d21
Benjamin San Souci <benjamin.sansouci@gmail.com>
2018-08-09 23:34:54 -0700

    Big commit

--- jscomp/bsb/bsb_ninja_native.mli
+++ jscomp/bsb/bsb_ninja_native.mli
@@ -1,5 +1,5 @@
-(* Copyright (C) 2017 Authors of BuckleScript
- *
+(* Copyright (C) 2015-2016 Bloomberg Finance L.P.
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -17,10 +17,32 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
 
+#if BS_NATIVE then
 
-val query: cwd:string -> bsc_dir:string -> string -> unit  
+(* File following almost the same logic as bsb_ninja but slightly modified for 
+   native / bytecode compilation. *)
+type compile_target_t = Native | Bytecode
+  
+val handle_file_groups : out_channel ->
+  custom_rules:Bsb_rule.t String_map.t -> 
+  is_top_level:bool ->
+  build_library:string option ->
+  compile_target:compile_target_t ->
+  backend:Bsb_config_types.compilation_kind_t ->
+  dependency_info:Bsb_dependency_info.t ->
+  ocaml_lib:string ->
+  root_project_dir:string ->
+  config:Bsb_config_types.t ->
+  ppx_flags_internal:string list ->
+  ?build_just_ppx: bool ->
+  Bsb_file_groups.file_group list ->
+  string option ->
+  Bsb_ninja_file_groups.info -> 
+  Bsb_ninja_file_groups.info
+  
+#end
