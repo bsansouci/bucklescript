558db7fd972970e466aab4cbe93b16a5920a90ba
Avery Morin <avery.schmavery@gmail.com>
2018-11-11 20:38:33 -0800

    More fixes for linux build
    Also automatically pick up version :)

--- scripts/linux-build-helper.sh
+++ scripts/linux-build-helper.sh
@@ -5,6 +5,9 @@ set -ex
 # don't call this script directly, call linux-build.sh instead #
 ################################################################
 
+VERSION=$(grep 'version' /io/package.json | sed -r 's/.*([0-9.]+.[0-9]+.[0-9]+).*/\1/')
+echo Building version $VERSION
+
 # install zip in order to create the zip package later
 yum install -y zip git
 
@@ -14,7 +17,7 @@ source /hbb/activate
 # build ocaml
 cd /io/vendor/ocaml
 DIRNAME=`pwd`
-/io/vendor/ocaml/configure -prefix `pwd` -cc "gcc -fPIC" -aspp "gcc -c -fPIC" -no-ocamldoc -no-ocamlbuild -no-curses -no-graph -no-debugger
+/io/vendor/ocaml/configure -prefix `pwd` -cc "gcc -fPIC" -aspp "gcc -c -fPIC" -no-ocamldoc -no-ocamlbuild -no-curses -no-graph
 make clean
 make world.opt
 make install
@@ -23,14 +26,14 @@ make -C otherlibs/systhreads
 cp otherlibs/systhreads/threads.h $DIRNAME/lib/ocaml/caml/threads.h
 
 # copy ninja binary
-cp /io/vendor/ninja-build/ninja.linux64 /io/lib/ninja.exe
+cp /io/vendor/ninja/snapshot/ninja.linux /io/lib/ninja.exe
 
 # build bsc/bsb
 cd /io
 PATH=/io/vendor/ocaml:$PATH make -C jscomp clean
-PATH=/io/vendor/ocaml:$PATH make
+PATH=/io/vendor/ocaml:$PATH make world-native
 PATH=/io/vendor/ocaml:$PATH make install
 
 # create zip package
-rm -f bsb-native-linux-2.1.1.zip
-zip -r bsb-native-linux-2.1.1.zip lib vendor/ocaml/bin/ocamlrun vendor/ocaml/ocamlc.opt vendor/ocaml/ocamlopt.opt vendor/ocaml/lib/ocaml -x lib/bsb -x lib/bsc -x lib/bsrefmt
+rm -f bsb-native-linux-${VERSION}.zip
+zip -r bsb-native-linux-${VERSION}.zip lib vendor/ocaml/bin/ocamlrun vendor/ocaml/ocamlc.opt vendor/ocaml/ocamlopt.opt vendor/ocaml/lib/ocaml -x lib/bsb -x lib/bsc -x lib/bsrefmt
