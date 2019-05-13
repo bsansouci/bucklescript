44014a4d1b0f9d9e3205eb64b75ee2319e9ebb6c
Benjamin San Souci <benjamin.sansouci@gmail.com>
2019-05-13 00:41:28 -0700

    Fixes to release script

--- scripts/bsb_native_release.sh
+++ scripts/bsb_native_release.sh
@@ -5,9 +5,9 @@ set -e
 export MACOSX_DEPLOYMENT_TARGET="10.10"
 
 # Make sure the global ocaml is up to date
-opam update
-opam switch reinstall 4.02.3+buckle-master
-eval `opam config env`
+# opam update
+# opam switch reinstall 4.02.3+buckle-master
+# eval `opam config env`
 
 # Don't run ./scripts/buildocaml.sh because that turns off some features we actually want.
 cd vendor/ocaml
@@ -23,12 +23,14 @@ make common/bs_version.ml
 make core/js_map.ml
 make core/js_fold.ml
 make force-snapshotml-native
+make BS_NATIVE=true -C belt_byte
+make BS_NATIVE=true -C belt_native
 cd ..
 
 # Get version number from package.json
 VERSION=$(cat package.json | sed -n -E 's/.*"version": "(.*)",/\1/p')
 
-# We need to run this because we need to make sure everything in bsb-native gets compiled with the 
+# We need to run this because we need to make sure everything in bsb-native gets compiled with the
 # vendored ocaml and not the global one. The reason is that the vendored one has some changes to be
 # relocable that weren't published to the global one.
 node scripts/install.js native
